77bf341af0efefae9a8e427891b5e0df9779e055
David Diamanta <dantadiamanta@gmail.com>
2023-05-11 03:53:30 +0700

    nameText

--- p8/p8/lib/theme.dart
+++ p8/p8/lib/theme.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
-Color ITColor = Color(0xff247699);
-Color lightITColor = Color(0xff9ED0FF);
+Color primaryColor = Color(0xff6319B8);
+Color lightColor = Color(0xff9ED0FF);
 Color blackColor = Color(0xff2C3A59);
 Color greyColor = Color(0xff808BA2);
 Color whiteColor = Color(0xffFFFFFF);
+
+TextStyle nameText = TextStyle(
+  color: blackColor,
+  fontWeight: FontWeight.w700,
+  fontSize: 20,
+);
 
 TextStyle titleText = TextStyle(
   color: blackColor,
