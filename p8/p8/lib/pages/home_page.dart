6ba3e17dd1b22e2b8a46b9a238ed7501f39cc744
David Diamanta <dantadiamanta@gmail.com>
2023-05-11 03:22:41 +0700

    add blank code

---
+++ p8/p8/lib/pages/home_page.dart
@@ -0,0 +1,26 @@
+import 'package:p8/theme.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ITColor,
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 40,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ p8/p8/lib/pages/splash.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:p8/theme.dart';
+import 'home_page.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: whiteColor,
+      body: Center(
+        child: ListView(
+          //padding buat bungkus di listview
+          padding: EdgeInsets.all(25),
+          children: [
+            
+            
+            
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
